e50d4a410be3087dacca2c4128d80fa583b58ab8
Makktu <johnmcna@gmail.com>
2023-02-08 20:30:21 +0000

    reorg for next stage

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,11 @@
 import { useState } from "react";
-import MyButton from "./MyButton";
+import DayOne from "./dayone/DayOne";
 import "./App.css";
 
 const App = () => {
-  // !
-
-  const [count, setCount] = useState(0);
-
-  const handleButtonClick = () => {
-    setCount(count + 1);
-  };
-
-  const products = [
-    { title: "Cabbage", id: 1, like: false, magic: true },
-    { title: "Garlic", id: 2, like: true, magic: true },
-    { title: "Apple", id: 3, like: true, magic: false },
-  ];
-
   return (
     <>
-      <div className="paragraphs__default">
-        <p>Hello John.</p>
-        <p>
-          {products.map((product) => (
-            <li
-              key={product.id}
-              style={{
-                color: product.like ? "yellowgreen" : "orange",
-                textDecoration: product.magic ? "underline" : null,
-              }}
-            >
-              {product.title}
-            </li>
-          ))}
-        </p>
-        <MyButton count={count} onClick={handleButtonClick} />
-        <MyButton count={count} onClick={handleButtonClick} />
-        <MyButton count={count} onClick={handleButtonClick} />
-        <MyButton count={count} onClick={handleButtonClick} />
-      </div>
+      <DayOne />
     </>
   );
 };
